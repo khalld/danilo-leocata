10d1f17b7d547e92f15a8fe4100017e54ba7c628
Danilo <37438774+khalld@users.noreply.github.com>
2025-09-16 08:39:08 +0000

    Modularizzato

---
+++ src/components/resume/Body.svelte
@@ -0,0 +1,29 @@
+<script>
+    import BodyResumeSectionSummaryWrapper from "./section/Summary.svelte";
+    import ExperienceSectionWrapper from "./section/Experience.svelte";
+    import Skills from "./section/Skills.svelte";
+    import Education from "./section/Education.svelte";
+    import Reference from "./section/Reference.svelte";
+    import Language from "./section/Language.svelte";
+    import Interests from "./section/Interests.svelte";
+</script>
+
+<div class="resume-body p-5">
+    <BodyResumeSectionSummaryWrapper/>
+    <div class="row">
+        <div class="col-lg-9">
+            <ExperienceSectionWrapper/>
+        </div>
+        <div class="col-lg-3">
+            <Skills/>
+
+            <Education/>
+            
+            <Reference/>
+
+            <Language/>
+            
+            <Interests/>
+        </div>
+    </div><!--//row-->
+</div><!--//resume-body-->
---
+++ src/components/resume/Wrapper.svelte
@@ -0,0 +1,19 @@
+<script>
+    import BodyResumeWrapper from "./Body.svelte";
+    import HeaderResumeWrapper from "./Header.svelte";
+
+</script>
+
+<style>
+    article {
+       font-family: 'Roboto Variable', sans-serif;
+       font-size: 12px;
+    }
+</style>
+
+<article class="resume-wrapper text-center position-relative">
+	<div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+		<HeaderResumeWrapper/>
+		<BodyResumeWrapper/>
+	</div>
+</article> 
